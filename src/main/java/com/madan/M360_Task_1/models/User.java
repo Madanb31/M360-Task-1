f96ed1060d1026e9a05090869ddbc459a2cbbbec
Madanb31 <madangowda304@gmail.com>
2026-02-11 09:40:53 +0530

    changed id type to UUID

--- src/main/java/com/madan/M360_Task_1/models/User.java
+++ src/main/java/com/madan/M360_Task_1/models/User.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
@@ -14,7 +16,7 @@ public class User {
 
     @Id
     @GeneratedValue
-    private Long id;
+    private UUID id;
 
     private String name;
 
