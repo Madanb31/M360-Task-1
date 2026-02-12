65265562ebb05fabd1e75578235a4db89c1f8103
Madanb31 <madangowda304@gmail.com>
2026-02-12 12:25:35 +0530

    Added address and roles.

--- src/main/java/com/madan/M360_Task_1/models/User.java
+++ src/main/java/com/madan/M360_Task_1/models/User.java
@@ -1,17 +1,20 @@
 package com.madan.M360_Task_1.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString(exclude = "roles")
+@EqualsAndHashCode(exclude = "roles")
 public class User {
 
     @Id
@@ -23,7 +26,17 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-
     private String contactNum;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(unique = true)
+    private Address address;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
 }
